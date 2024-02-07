5cd0237593d371144f39b7419f0029541c8846e9
Nuno Fernandes <nmpf.2005@gmail.com>
2024-02-07 01:12:39 +0000

    Initial version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,7 +17,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='{inter.className} bg-gray-950'>
+        <nav className="sticky top-0 left-0 flex py-4 px-16 m-4 space-x-4 items-center text-white">
+          <Link className="ring-2 ring-gray-600 shadow-md hover:ring-gray-300 duration-300 transition  p-1 rounded-md" href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </nav>
+        <ul className="background">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        {children}
+      </body>
     </html>
   );
 }
